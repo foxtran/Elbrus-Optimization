f15816e36897473fbe2597490b39a1c4a7dfb07a
Igor S. Gerasimov <i.s.ger@ya.ru>
2020-12-31 22:30:43 +0300

    Parallel work with all orbitals

--- correlate.f90
+++ correlate.f90
@@ -6,7 +6,7 @@ program main
   real(fp), parameter :: deltaMoment = 1._fp / npointmoment
   integer,  parameter :: LU_density = 10, LU_orbital = 11
   ! arguments
-  integer :: arguments_count
+  integer :: arguments_count, orbitals_count
   integer :: stat
   logical :: existance
   character(len=:), allocatable :: program_name, density_file, orbital_file
@@ -18,7 +18,7 @@ program main
   ! work tables
   real(fp), dimension(moments) :: powN
   real(fp), dimension(:), allocatable :: radius, density, radius2
-  real(fp), dimension(:), allocatable :: correlation, orbital
+  real(fp), dimension(:,:), allocatable :: correlation, orbitals
   real(fp), dimension(:,:), allocatable :: density_table
   arguments_count = command_argument_count()
   if (arguments_count .le. 1) then
@@ -28,7 +28,9 @@ program main
     print *, "Stopping..."
     stop
   end if
+  orbitals_count = arguments_count - 1
   allocate(character(len=1024) :: density_file)
+  allocate(character(len=1024) :: orbital_file)
   call get_command_argument(1, density_file, status=stat)
   inquire(file=density_file, exist=existance)
   if(.not.existance) then
@@ -40,14 +42,30 @@ program main
   open(LU_density, file=density_file, form='unformatted', access='sequential')
   read(LU_density) radial_points
   allocate(radius(radial_points), density(radial_points), radius2(radial_points))
-  allocate(correlation(moments), orbital(radial_points))
+  allocate(correlation(moments, orbitals_count), orbitals(radial_points, orbitals_count))
   allocate(density_table(radial_points, moments))
-  allocate(character(len=1024) :: orbital_file)
   ! read density
   read(LU_density) start
   read(LU_density) shift
   read(LU_density) density
   close(LU_density)
+  do j = 1, orbitals_count
+    call get_command_argument(j+1, orbital_file, status=stat)
+    inquire(file=orbital_file, exist=existance)
+    if(.not.existance) then
+      print *, "Orbital file '" // trim(orbital_file) // "' is not exist!"
+      print *, "Please, check your input!"
+      print *, "Stopping..."
+      stop
+    end if
+    ! read orbitals
+    open(LU_orbital, file=orbital_file, form='unformatted', access='sequential')
+    read(LU_orbital) tmp_rp
+    read(LU_orbital) tmp_st
+    read(LU_orbital) tmp_sh
+    read(LU_orbital) orbitals(:,j)
+    close(LU_orbital)
+  end do
   ! prepare constant arrays
   do i = 1, radial_points
     radius(i) = start + shift*(i-1)
@@ -62,30 +80,18 @@ program main
     density_table(:,n) = density_table(:,n)/sum(density_table(:,n))
   end do
   ! orbitals
-  do j = 2, arguments_count
-    call get_command_argument(j, orbital_file, status=stat)
-    inquire(file=orbital_file, exist=existance)
-    if(.not.existance) then
-      print *, "Orbital file '" // trim(orbital_file) // "' is not exist!"
-      print *, "Please, check your input!"
-      print *, "Stopping..."
-      stop
-    end if
-    ! read orbitals
-    open(LU_orbital, file=orbital_file, form='unformatted', access='sequential')
-    read(LU_orbital) tmp_rp
-    read(LU_orbital) tmp_st
-    read(LU_orbital) tmp_sh
-    read(LU_orbital) orbital
-    close(LU_orbital)
+  do j = 1, orbitals_count
     ! r^2 weighing and normalizing orbital
-    orbital = radius2 * orbital * orbital
-    orbital = orbital/sum(orbital)
+    orbitals(:,j) = radius2 * orbitals(:,j) * orbitals(:,j)
+    orbitals(:,j) = orbitals(:,j)/sum(orbitals(:,j))
     do n = 1, moments
-      correlation(n) = sum(abs(orbital-density_table(:,n)))
+      correlation(n,j) = sum(abs(orbitals(:,j)-density_table(:,n)))
     end do
+  end do
+  do j = 1, orbitals_count
+    call get_command_argument(j+1, orbital_file, status=stat)
     do n = 1, moments
-      write(6, "(A,A1,F4.1,A1,E18.12)") trim(orbital_file), ";", powN(n), ";", correlation(n)
+      write(6, "(A,A1,F4.1,A1,E18.12)") trim(orbital_file), ";", powN(n), ";", correlation(n,j)
     end do
   end do
 end program main
